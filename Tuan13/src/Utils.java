a09b0bd2b2c6e58d5d41d035c2e0bd124f587b71
LeHongTrieu <149747054+LeHongTrieu@users.noreply.github.com>
2023-12-10 23:30:00 +0700

    Add files via upload

---
+++ Tuan13/src/Utils.java
@@ -0,0 +1,124 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Utils {
+    public static void clearFileContent(String path) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(path))) {
+            writer.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String readContentFromFile(String path) {
+        StringBuilder s = new StringBuilder();
+        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = read.readLine()) != null) {
+                s.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s.toString();
+    }
+
+    public static void writeContentToFile(String path) {
+        List<String> lines = new ArrayList<>();
+
+        try {
+            lines = Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        BufferedWriter write = null;
+        Scanner sc = null;
+
+        try {
+            write = new BufferedWriter(new FileWriter(path, true));
+            sc = new Scanner(System.in);
+            String writeIn = sc.nextLine();
+            boolean check = true;
+            for (String s : lines) {
+                if (writeIn.equals(s)) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                write.write(writeIn);
+            } else {
+                clearFileContent(path);
+                for (int i = 0; i < lines.size(); i++) {
+                    if (lines.get(i).equals(writeIn)) {
+                        lines.remove(i);
+                        i--;
+                    }
+                }
+                lines.add(writeIn);
+                write.write(lines.get(0));
+                for (int i = 1; i < lines.size(); i++) {
+                    write.write("\n");
+                    write.write(lines.get(i));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (write != null) {
+                    write.close();
+                }
+                if (sc != null) {
+                    sc.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void appendContentToFile(String path) {
+        BufferedWriter write = null;
+        Scanner sc = null;
+
+        try {
+            write = new BufferedWriter(new FileWriter(path, true));
+            sc = new Scanner(System.in);
+            String writeIn = sc.nextLine();
+            write.write("\n");
+            write.write(writeIn);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (write != null) {
+                    write.close();
+                }
+                if (sc != null) {
+                    sc.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static File findFileByName(String folderPath, String fileName) {
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().equals(fileName)) {
+                    return file;
+                }
+            }
+        }
+        return null;
+    }
+}
